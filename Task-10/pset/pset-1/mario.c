3e286f598b81e2f657a30ec94492a1fdf399a349
rigvedrs <73471201+rigvedrs@users.noreply.github.com>
2020-11-30 01:26:01 +0530

    Create mario.c

---
+++ Task-10/pset/pset-1/mario.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<cs50.h>
+
+int main(void)
+{
+    for(int i=0;i<5;i++)
+    {
+
+        for(int j=0;j<=i;j++)
+        {
+
+            printf("#");
+
+        }
+
+        printf("\n");
+    }
+
+
+}
